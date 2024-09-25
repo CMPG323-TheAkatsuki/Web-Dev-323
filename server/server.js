3490c9912653acd3347fca9ce5a71727cf6f913c
Seither2k <kchindongo@outlook.com>
2024-09-25 13:55:23 +0200

    Back-end login
    The end points for the backend page have been made under the server folder
    
    GitHub\Web-Dev-323\server\db

--- server/server.js
+++ server/server.js
@@ -1,15 +1,22 @@
 import express from "express";
 import cors from "cors";
 import records from "./routes/record.js";
-
+import morgan from "morgan";
 const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/record", records);
+app.use(morgan('tiny'));
+app.disable('x-powered-by'); // less hackers know about the stack
 
+const port = 8080;
+/** HTTP GET Request */
+app.get('/', (req,res) => {
+    res.status(201).json("Home GET Request");
+});
 // start the Express server
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server connected to http://localhost:${PORT}`);
 });
