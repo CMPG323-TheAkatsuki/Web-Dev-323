7002a199bf37632d7793a334a837ba34138f2da3
Kwanele <141504413+BhunganekaNsele@users.noreply.github.com>
2024-09-27 14:34:39 +0200

    Revert "App/km"

--- server/server.js
+++ server/server.js
@@ -1,29 +1,15 @@
 import express from "express";
 import cors from "cors";
 import records from "./routes/record.js";
-import morgan from "morgan";
-import router from "./routes/routes.js";
+
 const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/record", records);
-app.use(morgan('tiny'));
-app.disable('x-powered-by'); // less hackers know about the stack
-
-const port = 8080;
-/** HTTP GET Request */
-app.get('/', (req,res) => {
-    res.status(201).json("Home GET Request");
-});
-
-/** api routes */
-app.use('/api',router)
-
-
 
 // start the Express server
 app.listen(PORT, () => {
-  console.log(`Server connected to http://localhost:${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
